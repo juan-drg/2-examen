58a478ab14a3e2a0c70f9ffb901371665854b6b1
Juan ACC <ing.juan.acc@gmail.com>
2019-09-27 11:49:52 -0500

    Inicio Proyecto notificaciones
    Inicio Proyecto notificaciones

---
+++ examen-3-academia-equipo2-notificaciones/src/main/java/com/everis/examen3academianotificaciones/Examen3AcademiaEquipo2NotificacionesApplication.java
@@ -0,0 +1,13 @@
+package com.everis.examen3academianotificaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen3AcademiaEquipo2NotificacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen3AcademiaEquipo2NotificacionesApplication.class, args);
+	}
+
+}
---
+++ examen-3-academia-equipo2-notificaciones/src/test/java/com/everis/examen3academianotificaciones/Examen3AcademiaEquipo2NotificacionesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.everis.examen3academianotificaciones;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Examen3AcademiaEquipo2NotificacionesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
