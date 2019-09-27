8a1bc81b0083ff9044668092ed497ec2e404b9c2
AlexisAng <alexiis1796.aa.aa@gmail.com>
2019-09-27 14:55:46 -0500

    Metodo insertar creado en proyecto pedidos

--- examen-3-academia-equipo2-pedidos/src/main/java/com/everist/examentres/pedidos/responses/PedidoResponse.java
+++ examen-3-academia-equipo2-pedidos/src/main/java/com/everist/examentres/pedidos/responses/PedidoResponse.java
@@ -1,5 +1,40 @@
 package com.everist.examentres.pedidos.responses;
 
-public class PedidoResponse {
 
+import com.everist.examentres.pedidos.model.Pedido;
+
+public class PedidoResponse {
+	
+	
+	private boolean successful;
+	private String message;
+	private Pedido value;
+	private int port;
+	public boolean isSuccessful() {
+		return successful;
+	}
+	public void setSuccessful(boolean successful) {
+		this.successful = successful;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Pedido getValue() {
+		return value;
+	}
+	public void setValue(Pedido value) {
+		this.value = value;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	
+	
 }
--- examen-3-academia-equipo2-pedidos/src/main/java/com/everist/examentres/pedidos/rest/PedidoController.java
+++ examen-3-academia-equipo2-pedidos/src/main/java/com/everist/examentres/pedidos/rest/PedidoController.java
@@ -1,8 +1,17 @@
 package com.everist.examentres.pedidos.rest;
 
+import java.util.Date;
+
+import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.everist.examentres.pedidos.model.Cliente;
+import com.everist.examentres.pedidos.model.Pedido;
+import com.everist.examentres.pedidos.model.Producto;
+import com.everist.examentres.pedidos.responses.PedidoResponse;
 import com.everist.examentres.pedidos.service.PedidoService;
 
 @RestController
@@ -10,5 +19,31 @@ public class PedidoController {
 	
 	@Autowired
 	private PedidoService pedidoService;
+	
+	@Autowired
+	private Environment environment;
+	
+	
+	@PostMapping("/pedido/cliente/{cliente}/producto/{producto}")
+	public PedidoResponse insertarPedido(@PathVariable int idcliente, @PathVariable int idproducto) {
+		
+		Pedido pedido = new Pedido();
+		
+		Cliente cliente = ClienteService.buscarCliente(idcliente);
+		Producto producto =   ProductoService.buscarProducto(idproducto);
+		
+		PedidoResponse response = new PedidoResponse();
+		
+		try {
+			pedido.setFechahoraentrega(new Date());
+			pedido.setFechahoraregistro(new Date());
+			pedido.setCliente(cliente.getIdcliente());
+			response.setSuccessful(true);
+			response.setMessage("Pedido Insertado");   
+		}catch (Exception e) {
+			response.setMessage("Error al insertar pedido");
+			response.setSuccessful(true);
+		}
+			}
 	
 }
