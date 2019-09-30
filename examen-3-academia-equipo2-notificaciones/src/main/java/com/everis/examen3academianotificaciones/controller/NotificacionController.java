45c1808b0ec9b397b5d1c667d830282c00a2f05d
Juan ACC <ing.juan.acc@gmail.com>
2019-09-30 13:51:20 -0500

    actualizacion notificacion get a post

--- examen-3-academia-equipo2-notificaciones/src/main/java/com/everis/examen3academianotificaciones/controller/NotificacionController.java
+++ examen-3-academia-equipo2-notificaciones/src/main/java/com/everis/examen3academianotificaciones/controller/NotificacionController.java
@@ -13,6 +13,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,7 +42,7 @@ public class NotificacionController {
 		return datosConfiguracion;
 	}
 
-	@GetMapping("/mensajes")
+	@PostMapping("/mensajes")
 	public List<MensajeResponse> mensajes(@RequestBody PedidoResponse pedidoResponse) {
 		// "(email,whatsapp,ambas)"
 //		if(datosConfiguracion.getEmaildestino() == null) {
